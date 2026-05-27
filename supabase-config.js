6e602c21aae7e705510700b72771a4864bea3e3c
sistemaduolmold <sistemaduolmold@gmail.com>
2026-05-27 15:20:45 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.DUOMOLD_SUPABASE = {
-  url: "https://wdgoqfixddwrgycplexi.supabase.co",
-  anonKey: "sb_publishable_Jdy98aMFJFUaZSOfT1YY4g_-3iYwiLb",
-  stateTable: "duomold_app_state",
-  stateId: "main"
-};
