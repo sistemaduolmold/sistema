9ed9572fbc028abe81539c837cd79d4e5c94e9fb
sistemaduolmold <sistemaduolmold@gmail.com>
2026-05-27 15:17:36 +0100

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.DUOMOLD_SUPABASE = {
+  url: "https://wdgoqfixddwrgycplexi.supabase.co",
+  anonKey: "sb_publishable_Jdy98aMFJFUaZSOfT1YY4g_-3iYwiLb",
+  stateTable: "duomold_app_state",
+  stateId: "main"
+};
